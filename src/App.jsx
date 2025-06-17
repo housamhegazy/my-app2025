d02b1f78f18bdc6915460ae6239be6f4a9c6c05c
housamhegazy <geohousam@gmail.com>
2025-06-17 22:04:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='title'>
+        hello iam comming soooooooon
+      </div>
+    </>
+  )
+}
+
+export default App
